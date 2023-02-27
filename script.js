7ae7b324517108a836d8357b7543a4a4f4055e23
Gonzalo <gonzaloprincipe18@gmail.com>
2023-02-26 23:52:24 -0300

    mi primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    link.className = "seleccionado";
+
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+
